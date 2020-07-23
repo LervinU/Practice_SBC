f00dfda76e0685bc5e3303be4c4029eb296617aa
Lervin Urena <l.urena@nexus.com.do>
2020-07-23 14:11:26 -0400

    enter and download .docx

--- capabilities.py
+++ capabilities.py
@@ -1,7 +1,10 @@
+from stuff import stuffs
+
 desiredCapabilities = {
-    "platformName": "Android",
-    "platformVersion": "9",
-    "automationName": "uiautomator2",
-    "deviceName": "emulator-5554",
-    "app": "APK PATH"
+        "platformName": "Android",
+        "platformVersion": "9",
+        "automationName": "uiautomator2",
+        "deviceName": "emulator-5554",
+        "browserName": "Chrome",
+        "chromedriverExecutable": stuffs["stuff3"]
 }
--- index.py
+++ index.py
@@ -1,6 +1,33 @@
 from appium import webdriver
 from capabilities import desiredCapabilities
+from appium.webdriver.common.touch_action import TouchAction
+from time import sleep
+from stuff import stuffs
 
 
 driver = webdriver.Remote("http://localhost:4723/wd/hub", desiredCapabilities)
 driver.implicitly_wait(30)
+
+
+def find_element_by_text(text):
+    return driver.find_element_by_xpath("//*[@text='" + text + "']")
+
+
+driver.get("https://unapec.edu.do")
+driver.find_elements_by_class_name("navbar-toggler-icon").__getitem__(0).click()
+driver.find_element_by_link_text("Aula virtual").click()
+TouchAction(driver).press(x=1255, y=1908).move_to(x=1255, y=1330).release().perform()
+driver.find_element_by_link_text("Ingresar aquí").click()
+driver.find_element_by_id("i0116").send_keys(stuffs["stuff1"])
+driver.find_element_by_id("idSIButton9").click()
+sleep(2)
+driver.find_element_by_id("i0118").send_keys(stuffs["stuff2"])
+sleep(2)
+driver.find_element_by_id("idSIButton9").click()
+sleep(2)
+driver.find_element_by_id("idBtn_Back").click()
+driver.find_element_by_link_text("SISTEMAS BASADOS EN EL CONOCIMIENTO - INF328-43111-001").click()
+driver.find_element_by_id("module-644979").click()
+
+
+
